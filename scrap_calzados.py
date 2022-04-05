116cdb945aac5e6a062d1522dbb02c5a5765177e
martinleal5 <102159844+martinleal5@users.noreply.github.com>
2022-04-05 18:15:49 +0200

    Programa para hacer el scrap de la web de calzados

---
+++ scrap_calzados.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 12:43:07 2022
+
+@author: martin
+"""
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from random import randint
+from time import sleep
+
+
+def transform_to_csv(data_frame):
+    """
+    Function that transforms data to pandas DataFrame object and
+    comma separated value.
+
+    :return: a pandas DataFrame object.
+    """
+    df = pd.DataFrame(data_frame)
+    df.to_csv('../data/sneakers.csv', sep=',',
+              encoding='utf-8', header=['id', 'name', 'model',
+                                        'actual price', 'old price', 'discount'])
+    return df
+
+# Creamos una lista en la que introducir la info útil que obtengamos
+sneakers_list = []
+
+# Recorremos las páginas de la web para extraer información de todos los zapatos
+for i in range(1,20):
+    r = requests.get(f'https://www.sivasdescalzo.com/es/calzado?p={i}')
+    soup = BeautifulSoup(r.content, 'lxml')
+
+    # Creamos lista de los productos
+    product_list = soup.find_all('div', class_ = 'product-card__info product details product-item-details')
+
+    # Extraemos la info que nos interesa de cada producto
+    for product in product_list:
+        name = product.find('a', class_ = 'set-product-storage').text
+        model = product.find('a', class_ = 'product-item-link set-product-storage').text
+        price_list = product.find('span', class_ = 'price').text.replace('\xa0€', '').split()
+        act_price = price_list[0]
+        try:
+            old_price = price_list[1]
+            try: 
+                discount = price_list[2]
+            except:
+                discount = '0%'
+                
+        except:
+            old_price = act_price
+            discount = '0%'
+        
+        # Almacenamos la info en un diccionario
+        sneaker = {
+            'name': name,
+            'model': model,
+            'actual price (€)': act_price,
+            'old price (€)': old_price,
+            'discount(%)': discount
+            }
+        sneakers_list.append(sneaker)
+    
+    # Esperamos un tiempo para realizar la siguiente búsqueda y evitar bloqueos
+    sleep(randint(1, 4))
+
+# Creamos un csv donde almacenar los datos
+transform_to_csv(sneakers_list)
