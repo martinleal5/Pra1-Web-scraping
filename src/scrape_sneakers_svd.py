d76916e1d95167f1899297f6750d4a8a7dc2cefa
eugepemi <al185399@uji.es>
2022-04-10 09:53:15 +0200

    Reformat code with Martin changes and new and definitive .csv

--- src/scrape_sneakers_svd.py
+++ src/scrape_sneakers_svd.py
@@ -3,76 +3,79 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from random import randint
 from time import sleep
+import os
 
 
 class Scraper:
     """
     Class to scrape the sneakers from Sivasdescalzo.com
     """
+
     sneakers_list = []
-    product_links = []
 
-    def set_product_links(self):
+    def __init__(self):
+        self.product_links = None
+
+    def set_product_attributes(self, product_info):
         """
-        Function that iterates through the shoe pages, obtaining a file in lxml format.
-        In this file it executes a search by href obtaining the links of each one of the products.
+        Function that iterates through HTTP requests, extracting the
+        different attributes needed.
         """
-        for page in range(0, 10):
-            r = requests.get(f'https://www.sivasdescalzo.com/es/calzado?p={page}')
-
-            soup = BeautifulSoup(r.content, 'lxml')
-            self.product_list = soup.find_all('div', class_='product-item-info product-card')
-
-            for item in self.product_list:
-                for link in item.find_all('a', href=True):
-                    self.product_links.append(link['href'])
-
-            sleep(randint(1, 8))
-
-    def get_product_links(self):
-        """
-        Product links getter.
-
-        :return: a list with the product links.
-        """
-        return self.product_links
-
-    def set_product_attributes(self):
-        """
-        Function that iterates through the different products obtaining an lmxl file,
-        where it performs a search and extracts its different attributes:
-        [name, model, price, discount, description]
-        """
-        for link in self.product_links:
-            r = requests.get(link)
-            soup = BeautifulSoup(r.content, 'lxml')
-            name = soup.find('span', class_='product-data__brand-name').text
-            model = soup.find('span', class_='product-data__model').text
-            price = soup.find('span', class_='price').text.replace('\xa0', '')
+        for products in product_info:
+            name = products.find('a', class_='set-product-storage').text
+            model = products.find('a', class_='product-item-link set-product-storage').text
+            # Here we get a list with different prices that we unpack, replace for character error.
+            prices = products.find('span', class_='price').text.replace('\xa0€', '').split()
+            act_price = prices[0]
             try:
-                discount = soup.find('span', class_='price-discount-percent').text
+                old_price = prices[1]
+                try:
+                    discount = prices[2]
+                except:
+                    discount = '0%'
             except:
-                discount = '0 %'
-            description = soup.find('div', class_='product-data__short-desc').text.replace('Más información',
-                                                                                           ' ').rstrip()
+                old_price = act_price
+                discount = '0%'
+
             sneakers = {
                 'name': name,
                 'model': model,
-                'price': price,
-                'discount': discount,
-                'description': description
+                'actual price (€)': act_price,
+                'old price (€)': old_price,
+                'discount': discount
             }
             print(sneakers)
             self.sneakers_list.append(sneakers)
-            sleep(randint(1, 2))
 
-    def get_attributes(self):
+    def scrape_product_info(self):
+        """
+        Function that iterates through the shoe pages doing HTTP requests,
+        to obtain a BeautifulSoup object to do searches by tag, scrapping
+        the information needed.
+        """
+        for page in range(1, 20):
+            r = requests.get(f'https://www.sivasdescalzo.com/es/calzado?p={page}')
+            soup = BeautifulSoup(r.content, 'lxml')
+            self.product_links = soup.find_all('div',
+                                               class_='product-card__info product details product-item-details')
+            self.set_product_attributes(self.product_links)
+            # Handling anti-scraping mechanism
+            sleep(randint(1, 4))
+
+    def get_product_attributes(self):
         """
         Product attributes getter.
 
         :return: a list with the product attributes.
         """
-        return self.product_list
+        return self.sneakers_list
+
+    def create_data_folder(self):
+        """
+        Function that creates a folder to store the scrapped data.
+        """
+        if not os.path.exists('../data/'):
+            os.mkdir('../data/')
 
     def transform_to_csv(self):
         """
@@ -82,14 +85,16 @@ class Scraper:
         :return: a pandas DataFrame object.
         """
         df = pd.DataFrame(self.sneakers_list)
-        df.to_csv('../data/sneakers.csv', sep=',',
-                  encoding='utf-8', header=['name', 'model',
-                                            'price', 'discount', 'description'])
+        self.create_data_folder()
+        df.to_csv('../data/SVD_sneakers.csv', sep=',',
+                  encoding='utf-8',
+                  header=['name', 'model',
+                          'actual price', 'old price', 'discount']
+                  )
         return df
 
 
 if __name__ == '__main__':
     scrape = Scraper()
-    scrape.set_product_links()
-    scrape.set_product_attributes()
+    scrape.scrape_product_info()
     scrape.transform_to_csv()
